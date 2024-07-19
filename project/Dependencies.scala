168eb28b7a29083b10b0dc9b2874996147190c8b
GaneshMaddipoti <16971000+GaneshMaddipoti@users.noreply.github.com>
2024-07-19 09:47:57 +0100

    NP-172: Added variable load to the requests.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,6 +6,8 @@ object Dependencies {
     "uk.gov.hmrc"          %% "performance-test-runner"   % "6.0.0"         % Test,
     "io.github.wolfendale"    %% "scalacheck-gen-regexp"     % "1.1.0"      % Test,
     "org.scalacheck"          %% "scalacheck"                % "1.18.0"     % Test,
+    "uk.gov.hmrc" %% "bootstrap-test-play-30"               % "9.0.0" % Test,
+  "com.lihaoyi" %% "upickle" % "3.3.1" %Test
   )
 
 }
---
+++ src/test/scala/uk/gov/hmrc/perftests/uknwauthcheckerapi/requests/UknwAuthCheckerApiRequests.scala
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.perftests.uknwauthcheckerapi.requests
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.http.request.builder.HttpRequestBuilder
+import io.netty.handler.codec.http.HttpResponseStatus
+import uk.gov.hmrc.performance.conf.ServicesConfiguration
+import uk.gov.hmrc.perftests.uknwauthcheckerapi.util.Helper
+
+object UknwAuthCheckerApiRequests extends ServicesConfiguration {
+
+  val baseUrl: String             = baseUrlFor("uknw-auth-checker-api")
+  val route: String               = "/authorisations"
+  private val bearerToken: String = if (runLocal) s"$${accessToken}" else s"Bearer $${accessToken}"
+
+  val postAuthorisation: HttpRequestBuilder =
+    http("Post Authorisations")
+      .post(s"$baseUrl$route": String)
+      .body(StringBody(Helper.singleEoriJsonBody))
+      .headers(
+        Map(
+          HttpHeaderNames.Authorization -> bearerToken,
+          HttpHeaderNames.Accept        -> "application/vnd.hmrc.1.0+json",
+          HttpHeaderNames.ContentType   -> "application/json"
+        )
+      )
+      .check(status.is(HttpResponseStatus.OK.code()))
+
+  val post100EoriAuthorisation: HttpRequestBuilder =
+    http("Post Authorisations")
+      .post(s"$baseUrl$route": String)
+      .body(StringBody(Helper.hundredEoriJsonBody))
+      .headers(
+        Map(
+          HttpHeaderNames.Authorization -> bearerToken,
+          HttpHeaderNames.Accept        -> "application/vnd.hmrc.1.0+json",
+          HttpHeaderNames.ContentType   -> "application/json"
+        )
+      )
+      .check(status.is(HttpResponseStatus.OK.code()))
+
+  val post500EoriAuthorisation: HttpRequestBuilder =
+    http("Post Authorisations")
+      .post(s"$baseUrl$route": String)
+      .body(StringBody(Helper.fiveHundredEoriJsonBody))
+      .headers(
+        Map(
+          HttpHeaderNames.Authorization -> bearerToken,
+          HttpHeaderNames.Accept        -> "application/vnd.hmrc.1.0+json",
+          HttpHeaderNames.ContentType   -> "application/json"
+        )
+      )
+      .check(status.is(HttpResponseStatus.OK.code()))
+
+  val post1000EoriAuthorisation: HttpRequestBuilder =
+    http("Post Authorisations")
+      .post(s"$baseUrl$route": String)
+      .body(StringBody(Helper.thousandEoriJsonBody))
+      .headers(
+        Map(
+          HttpHeaderNames.Authorization -> bearerToken,
+          HttpHeaderNames.Accept        -> "application/vnd.hmrc.1.0+json",
+          HttpHeaderNames.ContentType   -> "application/json"
+        )
+      )
+      .check(status.is(HttpResponseStatus.OK.code()))
+
+  val post3000EoriAuthorisation: HttpRequestBuilder =
+    http("Post Authorisations")
+      .post(s"$baseUrl$route": String)
+      .body(StringBody(Helper.threeThousandEoriJsonBody))
+      .headers(
+        Map(
+          HttpHeaderNames.Authorization -> bearerToken,
+          HttpHeaderNames.Accept        -> "application/vnd.hmrc.1.0+json",
+          HttpHeaderNames.ContentType   -> "application/json"
+        )
+      )
+      .check(status.is(HttpResponseStatus.OK.code()))
+}
