45a18e66c0ec80449dff5e6905f5a9ce05f03ea8
Daniel P. Doyle <3020488+TheDanielDoyle@users.noreply.github.com>
2024-11-21 13:52:20 +0000

    NP-Packages: Package updates

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"          %% "performance-test-runner" % "6.1.0"  % Test,
     "io.github.wolfendale" %% "scalacheck-gen-regexp"   % "1.1.0"  % Test,
-    "org.scalacheck"       %% "scalacheck"              % "1.18.0" % Test,
+    "org.scalacheck"       %% "scalacheck"              % "1.18.1" % Test,
     "uk.gov.hmrc"          %% "bootstrap-test-play-30"  % "9.5.0"  % Test,
     "com.lihaoyi"          %% "upickle"                 % "4.0.2"  % Test
   )
